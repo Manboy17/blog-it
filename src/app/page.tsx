d80fe21c8b816027d709094b71080f8facdcc628
denys <denia15042005@gmail.com>
2023-09-09 17:35:29 +0200

    Recent Posts Design Part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Categories from "@/components/Categories";
+import MostPopular from "@/components/MostPopular";
 import Navbar from "@/components/Navbar";
+import RecentPosts from "@/components/RecentPosts";
 
 export default function Home() {
   return (
     <div className="max-w-[1200px] mx-auto px-4">
       <Navbar />
       <Categories />
+      <div className="mt-12 flex gap-4">
+        <RecentPosts />
+        <MostPopular />
+      </div>
     </div>
   );
 }
---
+++ src/components/MostPopular.tsx
@@ -0,0 +1,11 @@
+const MostPopular = () => {
+  return (
+    <div className="w-1/3 p-4">
+      <div className="text-sm font-light text-gray-700">🔥 Here's hot</div>
+      <div className="text-lg font-semibold pb-4">Most Popular</div>
+      <div className="p-4">Hello</div>
+    </div>
+  );
+};
+
+export default MostPopular;
